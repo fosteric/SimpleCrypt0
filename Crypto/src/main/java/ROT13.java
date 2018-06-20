092d18e096979981f3c7fed873999a0e927aaa81
Eric Foster <ericf@zipcoders-MBP-3.localdomain>
2018-06-20 16:44:27 -0400

    attempting keypair test

---
+++ Crypto/src/main/java/ROT13.java
@@ -0,0 +1,164 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
+import java.io.*;
+import java.security.*;
+import java.util.Base64;
+
+public class ROT13  {
+
+    private int shift;
+
+    ROT13(Character cs, Character cf) {
+        this.shift = cf - cs;
+    }
+
+    ROT13() {
+        this.shift = 13;
+    }
+
+    public String readFileToString(String filename) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line = null;
+        while((line = br.readLine()) != null){
+            sb.append(line);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String cryptFile(String filename) throws IOException {
+        String s = readFileToString(filename);
+        return crypt(s);
+    }
+
+    public void writeFile(String filename, String content) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+        bw.write(content);
+        bw.close();
+    }
+
+    public String crypt(String text) throws UnsupportedOperationException {
+        return revolve(text, shift);
+    }
+
+    public String encrypt(String text) {
+        return revolve(text, shift);
+    }
+
+    public String decrypt(String text) {
+        return revolve(text, 26-shift);
+    }
+
+    public String revolve(String s, int revolve) {
+        char[] original = s.toCharArray();
+        char[] translated = new char[s.length()];
+        for (int i=0; i<original.length; i++) {
+            if (isLowerCase(original[i])) {
+                translated[i] = (char) ((((int) original[i] - (int) 'a' + revolve) % 26) + (int) 'a');
+            } else if (isUpperCase(original[i])) {
+                translated[i] = (char) ((((int) original[i] - (int) 'A' + revolve) % 26) + (int) 'A');
+            } else {
+                translated[i] = original[i];
+            }
+        }
+        return new String(translated);
+    }
+
+    public String rotate(String s, Character c){
+        if (c > 'A') {
+            return s.substring((s.length() - (c - 'A')), s.length()) + s.substring(0, s.length() - (c - 'A'));
+        } else {
+            return s;
+        }
+    }
+
+    public void AES() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        String key = "dAtAbAsE98765432";
+        String key2 = "dAtAbAsE98765432dAtAbAsE98765432";
+        String expected = "Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "   So long as men can breathe or eyes can see,\n" +
+                "   So long lives this, and this gives life to thee.\n";
+        Key aesKey = new SecretKeySpec(key2.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+        byte[] encrypted = cipher.doFinal(expected.getBytes());
+        String enc = new String(Base64.getEncoder().encodeToString(encrypted));
+        System.err.println("Encrypted: " + enc);
+        //During the decryption (Java 8):
+
+        System.out.print("Enter ciphertext: ");
+        encrypted = Base64.getDecoder().decode(enc);
+
+        cipher.init(Cipher.DECRYPT_MODE, aesKey);
+        String decrypted = new String(cipher.doFinal(encrypted));
+        System.err.println("Decrypted: " + decrypted);
+    }
+
+    public void publicPrivateKeyPair() throws NoSuchProviderException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        //Create a Key Pair Generator
+        KeyPairGenerator keyGenAlice = KeyPairGenerator.getInstance("DSA","SUN");
+        KeyPairGenerator keyGenBob = KeyPairGenerator.getInstance("DSA", "SUN");
+
+        //Initialize the Key Pair Generator
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        keyGenAlice.initialize(1024,random);
+        keyGenBob.initialize(1024,random);
+
+        //Generate the Pair of Keys
+        KeyPair keyPairAlice = keyGenAlice.generateKeyPair();
+        PrivateKey privAlice = keyPairAlice.getPrivate();
+        PublicKey pubAlice = keyPairAlice.getPublic();
+        KeyPair keyPairBob = keyGenBob.generateKeyPair();
+        PrivateKey privBob = keyPairBob.getPrivate();
+        PublicKey pubBob = keyPairBob.getPublic();
+
+        String expected = "Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "   So long as men can breathe or eyes can see,\n" +
+                "   So long lives this, and this gives life to thee.\n";
+        Key aesKey = new SecretKeySpec(pubAlice.toString().getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privAlice);
+        byte[] encrypted = cipher.doFinal(expected.getBytes());
+        String enc = new String(Base64.getEncoder().encodeToString(encrypted));
+        System.err.println("Encrypted: " + enc);
+        //During the decryption (Java 8):
+
+        System.out.print("Enter ciphertext: ");
+        encrypted = Base64.getDecoder().decode(enc);
+
+        cipher.init(Cipher.DECRYPT_MODE, aesKey);
+        String decrypted = new String(cipher.doFinal(encrypted));
+        System.err.println("Decrypted: " + decrypted);
+    }
+
+}
---
+++ Crypto/src/test/java/ROT13Test.java
@@ -0,0 +1,231 @@
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import static org.junit.Assert.*;
+
+public class ROT13Test {
+
+
+    @Test
+    public void rotateStringTest0() {
+        // Given
+        String s1 = "ABCDEF";
+        String s2 = "ABCDEF";
+
+        // When
+        ROT13 cipher = new ROT13();
+        String actual = cipher.rotate(s1, 'A');
+        System.out.println(s1);
+        System.out.println(actual);
+
+        // Then
+        assertTrue(actual.equals(s2));
+    }
+
+    @Test
+    public void rotateStringTest1() {
+        // Given
+        String s1 = "ABCDEF";
+        String s2 = "DEFABC";
+
+        // When
+        ROT13 cipher = new ROT13();
+        String actual = cipher.rotate(s1, 'D');
+        System.out.println(s1);
+        System.out.println(actual);
+
+        // Then
+        assertTrue(actual.equals(s2));
+    }
+
+    @Test
+    public void rotateStringTest2() {
+        // Given
+        String s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String s2 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+        // When
+        ROT13 cipher = new ROT13();
+        String actual = cipher.rotate(s1, 'N');
+        System.out.println(s1);
+        System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(s2));
+    }
+
+    @Test
+    public void cryptTest1() {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String Q1 = "Why did the chicken cross the road?";
+        String A1 = "Jul qvq gur puvpxra pebff gur ebnq?";
+
+        String Q2 = "Gb trg gb gur bgure fvqr!";
+        String A2 = "To get to the other side!";
+
+        // When
+        String actual = cipher.encrypt(Q1);
+        System.out.println(Q1);
+        System.out.println(A1);
+        System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(A1));
+
+        // When
+        String actual2 = cipher.decrypt(Q2);
+        System.out.println(Q2);
+        System.out.println(A2);
+        System.out.println(actual2);
+        // Then
+        assertTrue(actual2.equals(A2));
+    }
+    @Test
+    public void cryptTest2() {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String Q1 = "Why did the chicken cross the road?";
+        System.out.println(Q1);
+
+        // When
+        String actual = cipher.crypt(cipher.crypt(Q1));
+        System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(Q1));
+    }
+
+    @Test
+    public void cryptTxtFileTest() throws IOException {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String expected = "Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "   So long as men can breathe or eyes can see,\n" +
+                "   So long lives this, and this gives life to thee.\n";
+        System.out.println(expected);
+
+        // When
+        String actualE = cipher.cryptFile("sonnet18.txt");
+        String actual = cipher.decrypt(actualE);
+        System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void readFileWriteFileTest() throws IOException {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String expected = "Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "   So long as men can breathe or eyes can see,\n" +
+                "   So long lives this, and this gives life to thee.\n";
+        System.out.println(expected);
+
+        cipher.writeFile("sonnet18e.txt", expected);
+        String actual = cipher.readFileToString("sonnet18e.txt");
+
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void cryptTxtFileWriteTest() throws IOException {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String expected = "Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "   So long as men can breathe or eyes can see,\n" +
+                "   So long lives this, and this gives life to thee.\n";
+        System.out.println(expected);
+
+        // When
+        String actualE = cipher.cryptFile("sonnet18.txt");
+        cipher.writeFile("sonnet18e.txt", actualE);
+        String actual = cipher.readFileToString("sonnet18e.txt");
+        assertTrue(actualE.equals(actual));
+
+        String original = cipher.decrypt(actual);
+        assertTrue(original.equals(expected));
+        System.out.println(actual);
+    }
+
+    @Test
+    public void testAES() throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
+        ROT13 cipher = new ROT13('a', 'n');
+        try {
+            cipher.AES();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testPublicKeyPrivateKey() throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, InvalidKeyException {
+        ROT13 cipher = new ROT13('a', 'n');
+        try {
+            cipher.publicPrivateKeyPair();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
